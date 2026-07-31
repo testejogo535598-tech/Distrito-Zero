f4fe939c3b785ea71a86644e3131dfa1446f4e24
testejogo535598-tech <testejogo535598@gmail.com>
2026-07-31 17:44:50 -0300

    Create produtos.js

---
+++ src/data/produtos.js
@@ -0,0 +1,39 @@
+const produtos = [
+  {
+    id: 1,
+    nome: "AKM",
+    categoria: "Armas",
+    preco: 350,
+    moeda: "DZ Coins"
+  },
+  {
+    id: 2,
+    nome: "M4A1",
+    categoria: "Armas",
+    preco: 500,
+    moeda: "DZ Coins"
+  },
+  {
+    id: 3,
+    nome: "Humvee",
+    categoria: "Veículos",
+    preco: 3000,
+    moeda: "DZ Coins"
+  },
+  {
+    id: 4,
+    nome: "Armazém 5x5",
+    categoria: "Serviços",
+    preco: 1200,
+    moeda: "DZ Coins"
+  },
+  {
+    id: 5,
+    nome: "Bandeira de Base Inimiga",
+    categoria: "Compra",
+    preco: 1000,
+    moeda: "DZ Coins"
+  }
+];
+
+export default produtos;
