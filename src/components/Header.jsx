0665a2356e3da8a8639a45b5c866b103ff8f442a
testejogo535598-tech <testejogo535598@gmail.com>
2026-07-31 20:33:04 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 function Header() {
   return (
     <header>
-      <h1>Distrito Zero</h1>
-      <p>Mercado do servidor O Holocausto</p>
+      <div className="topo">
+        <h1>🏴 Distrito Zero</h1>
+        <p>Mercado Oficial do servidor O Holocausto</p>
+
+        <nav>
+          <button>🏪 Loja</button>
+          <button>🚗 Veículos</button>
+          <button>💣 Explosivos</button>
+          <button>📦 Serviços</button>
+          <button>💬 Discord</button>
+        </nav>
+      </div>
     </header>
   )
 }
