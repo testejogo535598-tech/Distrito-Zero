54a8d2646f7c2dddf9a2aff3db589555634cf220
testejogo535598-tech <testejogo535598@gmail.com>
2026-08-01 07:37:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Banner from './components/Banner'
+import ServicosPremium from './components/ServicosPremium'
 import Produtos from './components/Produtos'
 import Carrinho from './components/Carrinho'
 
