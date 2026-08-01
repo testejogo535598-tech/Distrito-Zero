3335b10db02fdfc87a561c938212c5437a590401
testejogo535598-tech <testejogo535598@gmail.com>
2026-07-31 22:19:13 -0300

    Create Banner.jsx

---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+function Banner() {
+  return (
+    <section className="banner">
+      <div className="banner-conteudo">
+        <h1>Distrito Zero</h1>
+
+        <p>Mercado Oficial do Servidor O Holocausto</p>
+
+        <div className="banner-botoes">
+          <button className="btn-comprar">
+            Entrar na Loja
+          </button>
+
+          <button className="btn-comprar">
+            Discord
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Banner
