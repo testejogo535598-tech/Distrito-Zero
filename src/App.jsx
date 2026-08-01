46080aface37f49e0af3135c6cd858f8d2493304
testejogo535598-tech <testejogo535598@gmail.com>
2026-07-31 21:16:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Produtos from './components/Produtos'
 import Carrinho from './components/Carrinho'
 
 function App() {
+  const [carrinho, setCarrinho] = useState([])
+
+  function adicionarAoCarrinho(produto) {
+    setCarrinho([...carrinho, produto])
+  }
+
+  function removerDoCarrinho(index) {
+    const novoCarrinho = [...carrinho]
+    novoCarrinho.splice(index, 1)
+    setCarrinho(novoCarrinho)
+  }
+
   return (
-    <div>
+    <>
       <Header />
-      <Produtos />
-      <Carrinho />
-    </div>
+
+      <Produtos
+        adicionarAoCarrinho={adicionarAoCarrinho}
+      />
+
+      <Carrinho
+        carrinho={carrinho}
+        removerDoCarrinho={removerDoCarrinho}
+      />
+    </>
   )
 }
 
