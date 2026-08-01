7e3726a6cb58d62fbfec484e804c13a7d925c3b0
testejogo535598-tech <testejogo535598@gmail.com>
2026-08-01 13:32:25 -0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,18 +3,24 @@ function Banner() {
     <section className="banner">
       <div className="banner-overlay">
 
+        <span className="status-online">
+          🟢 Servidor Online
+        </span>
+
         <h1>DISTRITO ZERO</h1>
 
-        <p>Mercado Oficial do Servidor O Holocausto</p>
+        <p>
+          Mercado Oficial do Servidor O Holocausto
+        </p>
 
         <div className="banner-botoes">
 
           <button className="btn-principal">
-            🛒 Entrar na Loja
+            Entrar na Loja
           </button>
 
           <button className="btn-secundario">
-            💬 Discord
+            Entrar no Discord
           </button>
 
         </div>
