441492fd79811cd4a7d22b66c5fa93baaaebaaac
testejogo535598-tech <testejogo535598@gmail.com>
2026-08-01 16:07:47 -0300

    Update produtos.js

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -4,36 +4,41 @@ const produtos = [
     nome: "AKM",
     categoria: "Armas",
     preco: 350,
-    moeda: "DZ Coins"
+    moeda: "DZ Coins",
+    imagem: "/imagens/akm.png"
   },
   {
     id: 2,
     nome: "M4A1",
     categoria: "Armas",
     preco: 500,
-    moeda: "DZ Coins"
+    moeda: "DZ Coins",
+    imagem: "/imagens/m4a1.png"
   },
   {
     id: 3,
     nome: "Humvee",
     categoria: "Veículos",
     preco: 3000,
-    moeda: "DZ Coins"
+    moeda: "DZ Coins",
+    imagem: "/imagens/humvee.png"
   },
   {
     id: 4,
     nome: "Armazém 5x5",
     categoria: "Serviços",
     preco: 1200,
-    moeda: "DZ Coins"
+    moeda: "DZ Coins",
+    imagem: "/imagens/armazem.png"
   },
   {
     id: 5,
     nome: "Bandeira de Base Inimiga",
-    categoria: "Compra",
+    categoria: "Serviços",
     preco: 1000,
-    moeda: "DZ Coins"
+    moeda: "DZ Coins",
+    imagem: "/imagens/bandeira.png"
   }
-];
+]
 
-export default produtos;
+export default produtos
