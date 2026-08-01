e09687c02ba376123573d09e0e0891795a853fd1
testejogo535598-tech <testejogo535598@gmail.com>
2026-08-01 08:30:06 -0300

    Create Rodape.jsx

---
+++ Rodape.jsx
@@ -0,0 +1,15 @@
+function Rodape() {
+  return (
+    <footer className="rodape">
+      <h3>Distrito Zero</h3>
+
+      <p>Mercado Oficial do Servidor O Holocausto</p>
+
+      <p>Discord • Regras • Suporte</p>
+
+      <small>© 2026 Distrito Zero. Todos os direitos reservados.</small>
+    </footer>
+  )
+}
+
+export default Rodape
