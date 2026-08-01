b0d7c3ba69755b6ee072c3eaa963ce3b0dbf9dea
testejogo535598-tech <testejogo535598@gmail.com>
2026-07-31 21:19:24 -0300

    Update Carrinho.jsx

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -1,11 +1,34 @@
-function Carrinho() {
+function Carrinho({ carrinho, removerDoCarrinho }) {
+  const total = carrinho.reduce((soma, item) => soma + item.preco, 0)
+
   return (
     <section>
       <h2>🛒 Carrinho</h2>
 
+      {carrinho.length === 0 ? (
+        <div className="card-produto">
+          <p>Seu carrinho está vazio.</p>
+        </div>
+      ) : (
+        carrinho.map((item, index) => (
+          <div className="card-produto" key={index}>
+            <h3>{item.nome}</h3>
+
+            <p>{item.preco} {item.moeda}</p>
+
+            <button
+              className="btn-comprar"
+              onClick={() => removerDoCarrinho(index)}
+            >
+              Remover
+            </button>
+          </div>
+        ))
+      )}
+
       <div className="card-produto">
-        <p>Seu carrinho está vazio.</p>
-        <p>Total: <strong>0 DZ Coins</strong></p>
+        <h3>Total</h3>
+        <p>💰 {total} DZ Coins</p>
 
         <button className="btn-comprar">
           Finalizar Compra
