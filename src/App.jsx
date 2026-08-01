2bb1ff47e0721ae564d8e7f479e14e20eaef3ac4
testejogo535598-tech <testejogo535598@gmail.com>
2026-07-31 21:08:57 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Produtos from './components/Produtos'
+import Carrinho from './components/Carrinho'
 
 function App() {
   return (
     <div>
       <Header />
       <Produtos />
+      <Carrinho />
     </div>
   )
 }
